b850c9cc0b3951784d5db18ae020d22da4ab6f40
MJ Richardson <michael.james.richardson@gmail.com>
2014-04-24 13:18:45 +1000

    Still attempting to get bind working

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -855,7 +855,7 @@ function checkboxInputType(scope, element, attr, ctrl) {
 var inputDirective = ['$browser', '$sniffer', function($browser, $sniffer) {
   return {
     restrict: 'E',
-    require: '?ngModel',
+    require: '?acuteBind',
     link: function(scope, element, attr, ctrl) {
       if (ctrl) {
         (inputType[lowercase(attr.type)] || inputType.text)(scope, element, attr, ctrl, $sniffer,
